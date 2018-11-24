de720bfa3ebc92889faf94183a1d35d7b7a95124
Bob-Utveckling <development@bamshad.se>
2018-11-24 14:34:36 +0100

    A first upload of files

---
+++ GuessingGame.java
@@ -0,0 +1,11 @@
+
+public class GuessingGame {
+
+
+   public static void main (String[] args) {
+      System.out.println("Test 1");
+      Guesser newGuesser = new Guesser(0,1000);
+      newGuesser.start();
+   }
+
+}
